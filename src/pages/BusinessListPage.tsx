427243a9ddc40fc517328d633534ae0670d2b937
polinakremneva <polinaapolly@gmail.com>
2024-07-21 09:59:53 +0300

    get all businesses and business by id

--- src/pages/BusinessListPage.tsx
+++ src/pages/BusinessListPage.tsx
@@ -28,6 +28,8 @@ import { GiRotaryPhone } from "react-icons/gi";
 import { FaClock } from "react-icons/fa";
 import { TbWorldWww } from "react-icons/tb";
 
+import api from "@/services/api.service";
+
 const categories = [
   { label: "Nearby", icon: <FaMapMarkerAlt /> },
   { label: "Top Rated", icon: <FaStar /> },
@@ -40,102 +42,51 @@ const categories = [
   { label: "Free WiFi", icon: <FaWifi /> },
 ];
 
-const shop: Business = {
-  _id: "66993caafa57e0d1c3f6c2dc",
-  name: "Café Roastery",
-  image:
-    "https://images.pexels.com/photos/887723/pexels-photo-887723.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  description: "A great place to drink coffee",
-  category: "Cafe",
-  contactInfo: {
-    address: "123 Coffee St.",
-    openAt: "09:00",
-    closeAt: "22:00",
-    phoneNumber: "123-456-7890",
-    websiteLink: "https://coffeeshop.com",
-  },
-  rating: 4.5,
-  reviews: ["66993caafa57e0d1c3f6c2df"],
-  summOfReviews: 30,
-};
-
-const bakery: Business = {
-  _id: "7709e3cfd3e1b8c4a9f7e3d9",
-  name: "Sweet Delights Bakery",
-  image:
-    "https://images.pexels.com/photos/205961/pexels-photo-205961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  description: "Delicious cakes and pastries made fresh daily.",
-  category: "Bakery",
-  contactInfo: {
-    address: "456 Cake Ave.",
-    openAt: "08:00",
-    closeAt: "18:00",
-    phoneNumber: "987-654-3210",
-    websiteLink: "https://sweetdelightsbakery.com",
-  },
-  rating: 4.8,
-  reviews: ["7709e3cfd3e1b8c4a9f7e3d9"],
-  summOfReviews: 30,
-};
-
-const pizzaPlace: Business = {
-  _id: "881a1f9e16b2c3d4f0e1b5c6",
-  name: "Bella Italia Pizzeria",
-  image:
-    "https://images.pexels.com/photos/23017572/pexels-photo-23017572/free-photo-of-pizza-with-basil-leaves.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  description: "Authentic Italian pizza with a variety of toppings.",
-  category: "Restaurant",
-  contactInfo: {
-    address: "789 Pizza Lane",
-    openAt: "11:00",
-    closeAt: "23:00",
-    phoneNumber: "555-123-4567",
-    websiteLink: "https://bellaitaliapizzeria.com",
-  },
-  rating: 4.7,
-  reviews: ["881a1f9e16b2c3d4f0e1b5c6"],
-  summOfReviews: 30,
-};
-
-const sushiBar: Business = {
-  _id: "992b2f8c6e4a1b2d3f5c6e4a",
-  name: "Sakura Sushi Bar",
-  image:
-    "https://images.pexels.com/photos/2323391/pexels-photo-2323391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  description: "Fresh and delicious sushi made with the finest ingredients.",
-  category: "Restaurant",
-  contactInfo: {
-    address: "101 Sushi St.",
-    openAt: "10:00",
-    closeAt: "22:00",
-    phoneNumber: "321-654-9870",
-    websiteLink: "https://sakurazushi.com",
-  },
-  rating: 4.6,
-  reviews: ["992b2f8c6e4a1b2d3f5c6e4a"],
-  summOfReviews: 30,
-};
-
 const BusinessListPage: React.FC = () => {
   const navigate = useNavigate();
-  const [businesses, setBusinesses] = useState<Business[]>([
-    shop,
-    bakery,
-    pizzaPlace,
-    sushiBar,
-  ]);
+  const [businesses, setBusinesses] = useState<Business[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+  const toggleDescription = (index: number) => {
+    if (expandedIndex === index) {
+      setExpandedIndex(null);
+    } else {
+      setExpandedIndex(index);
+    }
+  };
 
   const handleCardClick = (id: string) => {
     navigate(`/business/${id}`);
   };
 
   useEffect(() => {
-    setBusinesses(businesses);
-  });
+    const fetchBusinesses = async () => {
+      try {
+        const response = await api.get("/businesses");
+        setBusinesses(response.data);
+      } catch (err) {
+        setError("Failed to fetch businesses");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBusinesses();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <div className="bg-gradient-to-r from-purple-50 via-pink-50 to-red-50 min-h-screen py-8">
-      <div className="container mx-auto px-4">
+      <div className=" lg:px-[5em]">
         <div className="relative">
           <h1 className="text-4xl font-bold w-fit text-gray-800 mb-4 transition-transform transform hover:scale-105">
             Uncover Remarkable Places
@@ -197,22 +148,37 @@ const BusinessListPage: React.FC = () => {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          {businesses.map((business) => (
+          {businesses.map((business, index) => (
             <div
               key={business._id}
-              className="relative bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl"
-              onClick={() => handleCardClick(business._id)}
+              className=" bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl"
             >
               <img
                 src={business.image}
                 alt={business.name}
-                className="w-full h-48 object-cover rounded-t-lg"
+                onClick={() => handleCardClick(business._id)}
+                className="w-full h-48 cursor-pointer object-cover rounded-t-lg"
               />
               <div className="p-4">
-                <h2 className="text-xl font-semibold text-gray-800 mb-2 hover:text-pink-600 transition-colors duration-300">
+                <h2
+                  className="text-xl font-semibold cursor-pointer text-gray-800 mb-2 hover:text-pink-600 transition-colors duration-300"
+                  onClick={() => handleCardClick(business._id)}
+                >
                   {business.name}
                 </h2>
-                <p className="text-gray-600 mb-2">{business.description}</p>
+                <p className="text-gray-700 mb-4">
+                  {expandedIndex === index
+                    ? business.description
+                    : `${business.description.substring(0, 100)}...  `}
+                  <button
+                    onClick={() => toggleDescription(index)}
+                    className="text-cyan-800 font-semibold underline mb-4 lg:pl-3 hover:underline"
+                  >
+                    {expandedIndex === index
+                      ? "Close description"
+                      : "Show More"}
+                  </button>
+                </p>
                 <p className="text-gray-500 mb-2">
                   <span className=" font-semibold text-[1.1em] text-black">
                     Category:{" "}
